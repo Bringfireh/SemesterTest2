f5b50c87e6b9432956033e17d5201e210888f2cf
Bringfireh <ngutortsenongo@gmail.com>
2022-05-05 11:01:10 +0100

    completed the app

--- SemesterTest2/Program.cs
+++ SemesterTest2/Program.cs
@@ -10,6 +10,60 @@ namespace SemesterTest2
     {
         static void Main(string[] args)
         {
+            //1	Create a data structure e.g. ArrayList or List that must store 5 objects of type ThirdYearStudent
+
+            // Using List
+           // List<ThirdYearStudent> tyStudents = new List<ThirdYearStudent>();
+
+            // Using ArrayList (this is the one implemented).
+            ThirdYearStudent[] tyStudents = new ThirdYearStudent[5];
+
+            //2. Read Inputs -	Prompt the use for the following
+             for(int i=0; i < 5; i++)
+            {
+
+                Console.Write("Enter Student Number: ");
+                int studentNumber = Convert.ToInt16(Console.ReadLine());
+
+                Console.Write("Enter Student Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter Surname: ");
+                string surname = Console.ReadLine();
+
+                Console.Write("Enter course code: ");
+                string courseCode = Console.ReadLine();
+
+                Console.Write("Enter assignment Mark: ");
+                int assignmentMark = Convert.ToInt16(Console.ReadLine());
+
+                Console.Write("Enter Test Mark: ");
+                int testMark = Convert.ToInt16(Console.ReadLine());
+
+
+                //setting the object with user data.
+                ThirdYearStudent tyStudent = new ThirdYearStudent();
+                tyStudent.setStudent(studentNumber, name, surname, courseCode, assignmentMark, testMark);
+                tyStudents[i] = tyStudent;
+                Console.WriteLine("=======================================================================");
+            }
+            Console.WriteLine("");
+
+
+            //Displaying the data  
+            Console.WriteLine("Displaying Students data");
+            for(int j = 0; j < 5; j++)
+            {
+                tyStudents[j].displayStudent();
+                Console.WriteLine("***************************");
+            }
+            Console.WriteLine("");
+
+
+            //Determine the average final mark of all the student’s objects populate in the data structure.
+            Console.WriteLine("Finding the average final Mark of all the Students is: " + tyStudents.Average(x => x.finMark));
+            ;
+
         }
     }
 }
---
+++ SemesterTest2/ThirdYearStudent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemesterTest2
+{
+    class ThirdYearStudent:Student
+    {
+        public string code;
+        public int assignment;
+        public int test;
+        public double yearMark;
+        public double finMark;
+
+        public ThirdYearStudent()
+        {
+            code = null;
+            assignment = 0;
+            test = 0;
+        }
+
+        public void setStudent(int studentNumber, string name, string surname,string subjectCode, int assignmentMark, int testMark)
+        {
+            setStudent(studentNumber, name, surname);
+            code = subjectCode;
+            assignment = assignmentMark;
+            test = testMark;
+        }
+        public double calcYearMark(int assignment, int test)
+        {
+            double studentYearMark = (assignment * (45 / 100)) +( test * (55 / 100));
+            return studentYearMark;
+        }
+        public double finalMark(double yearMark)
+        {
+            Random random = new Random();
+            int examMark = random.Next(15, 99);
+            double final = (yearMark + examMark) / 2;
+            return final;
+        }
+        public override void displayStudent()
+        {
+            Console.WriteLine("\n" + studentNumber + " " + name + " " + surname);
+                yearMark = calcYearMark(assignment, test);
+                finMark = finalMark(yearMark);
+            Console.WriteLine("\n" + code + " " + assignment + " " + test + " " + yearMark + " " + finMark );
+
+        }
+    }
+}
